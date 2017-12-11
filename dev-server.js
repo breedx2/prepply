9ea9a406e18d832bf38e6073d5be155a4b876008
jason <jason@noisybox.net>
2017-12-10 19:41:01 -0800

    guard against inject on 404

--- dev-server.js
+++ dev-server.js
@@ -85,11 +85,12 @@ function startServer(args){
   const reloadInjector = interceptor((req, res) => ({
       // Only HTML responses will be intercepted
       isInterceptable: function(){
-        return /text\/html/.test(res.get('Content-Type'));
+        return (res.statusCode < 300) && /text\/html/.test(res.get('Content-Type'));
       },
       // Appends a paragraph at the end of the response body
       intercept: function(body, send) {
         console.log(`Inject reload js ${req.url}`);
+        console.log(res.get('Content-Type'));
         const result = body.replace('<body>', '<body>\n<script src="/reload/reload.js"></script>');
         send(result);
       }
