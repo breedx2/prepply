84ad50b228f2782717d4020af352415e92e69924
jason <jason@noisybox.net>
2019-11-03 20:47:45 -0800

    allow passing env script to publish

--- bin/publish.sh
+++ bin/publish.sh
@@ -6,8 +6,29 @@
 
 set -e
 
+function usage() {
+	echo
+	echo "Usage: $0 [-e <envscript>]"
+	echo
+	echo "(envscript defaults to env.sh in this bin dir)"
+	echo
+}
+
 MYDIR=`dirname "$0"`
-source "${MYDIR}/env.sh"
+ENVSCRIPT="${MYDIR}/env.sh"
+
+while getopts ":h:e:" opt; do
+  case ${opt} in
+    e ) ENVSCRIPT="${OPTARG}"
+      ;;
+    \? ) usage
+		 exit 1
+      ;;
+  esac
+done
+
+echo "Sourcing environment from => ${ENVSCRIPT}"
+source "${ENVSCRIPT}"
 
 echo Starting rsync
 rsync -avv --progress --checksum "${BUILD_OUTPUT_DIR%/}/" "${RSYNC_HOST}:${RSYNC_CONTENT_DIR%/}/"
